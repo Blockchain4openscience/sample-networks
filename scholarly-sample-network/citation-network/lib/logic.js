4776358ac38942669d4bfce60432c3ac65415235
Brayan <brayanrodbajo@gmail.com>
2018-01-09 12:11:29 -0500

    adding reward and bibref format

--- scholarly-sample-network/citation-network/lib/logic.js
+++ scholarly-sample-network/citation-network/lib/logic.js
@@ -4,10 +4,23 @@
  * @transaction
  */
 function tradeCommodity(citation) {
-    citation.bibRef.citations = citation.bibRef.citations + 1;
+    citation.bibRef.citations++;
     return getAssetRegistry('org.acme.cite.BibRef')
         .then(function (assetRegistry) {
             return assetRegistry.update(citation.bibRef);
         });
 }
-
+/**
+ * Increase in one the reward 
+ * @param {org.acme.cite.Cite} citation - the citation to be processed
+ * @transaction
+ */
+function giveReward(citation) {
+    if (((citation.iniPage && citation.finPage) && citation.pieceTxt) && citation.reason){
+        citation.citer.reward++;
+        return getParticipantRegistry('org.acme.cite.Researcher')
+   		  .then(function (participantRegistry) {
+              return participantRegistry.update(citation.citer);
+           });
+    }
+}
